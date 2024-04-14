bfa880540b5dfa59d67fcd0523cde006e45c1152
Dawood Alkawaz <dalkawaz@gmail.com>
2024-04-14 21:00:13 +0300

    Solution

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+  return reversedWord === word;
 }
-
 /* 
   Add your pseudocode here
+
+  // 1. Initilize new empty string
+  // 2. Reverse loop the given word
+  // 3. Add current letter to the reversed word
+  // 4. Return the check of identical of given and reversed word
+
 */
 
 /*
   Add written explanation of your solution here
+  // Input: Hello
+  // Initialize reversedWord = ""
+  // Loop:  Begin with the length of "Hello" and go down i--
+  // Add current letter "o" to begining of reversedWord 
+  // Return the check
 */
 
 // You can run `node index.js` to view these console logs
